061d810a805e32ea9d37f857aa0a365bf97ddb56
TheImpaler <vklepic@yahoo.fr>
2019-11-10 16:20:56 +0100

    Add files via upload

--- neural_network/src/deepNN/Main.java
+++ neural_network/src/deepNN/Main.java
@@ -1,9 +1,11 @@
-package deepNN;
+package NeuralNetJAVA;
 
 public class Main {
 
     public static void main(String[] args) {
         Network network = new Network();
         System.out.println(network);
+        Integer[] inputs = {1,2,3};
+        network.trainNetwork();
     }
 }

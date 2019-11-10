c2e6f1b84fee53705e86b21abb9ae4fa907d5177
TheImpaler <vklepic@yahoo.fr>
2019-11-10 01:17:06 +0100

    Add files via upload

--- neural_network/src/deepNN/Node.java
+++ neural_network/src/deepNN/Node.java
@@ -1,11 +1,11 @@
-package deepNN;
+package NeuralNetJAVA;
 
 public class Node {
 
-    private Float input;
-    private Float output;
-    private Double weight;
-    private Integer bias;
+    private Double input;
+    private Double output;
+    private Double[] weights;
+    private Double bias;
     private static Integer id = 0;
     private Integer node_id;
 
@@ -14,31 +14,65 @@ public class Node {
         this.node_id = this.id;
     }
 
-    public Node() {
-        this.bias = 1;
+    public Node(int num_weights) {
+        // on connait le nombre d'output <=> nombre de weights par node
+        this.weights = new Double[num_weights];
+        this.bias = 1.0;
     }
 
-    public Float getInput() {
+    @Override
+    public String toString() {
+        String stringWeights = "";
+        for (double w: this.weights) {
+            stringWeights += String.valueOf(w)+" || ";
+        }
+        return "\n\t\t\tNode: "+"id: "+this.node_id + " || "+
+                "weight: "+stringWeights;
+    }
+
+    /*
+    GETTERS || SETTERS
+     */
+    public void setInput(Double input) {
+        this.input = input;
+    }
+
+    public Double getInput() {
         return this.input;
     }
 
-    public Float getOutput() {
+    public Double getOutput() {
         return this.output;
     }
 
-    public void setOutput(float output){
+    public void setOutput(Double output){
         this.output = output;
     }
 
+    public void setBias(double bias) {
+        this.bias = bias;
+    }
+
+    public Double getBias() {
+        return this.bias;
+    }
+
     public void setWeight(double w) {
-        this.weight = w;
+        // add weight to last memory case
+        for (int i = 0; i < this.weights.length; i++){
+            if (this.weights[i] == null) { //dès que case est NULL => flush weight value and OUT
+                this.weights[i] = w;
+                break;
+            }
+        }
+    }
+
+    public Double[] get_weights() {
+        return this.weights;
     }
 
     public Integer getNode_id() {
         return this.node_id;
     }
-
-    public String toString() {
-        return "Node id: "+this.node_id;
-    }
 }
+
